291585e08d04a9e7671be83fddbd101aa07ec783
Arif Hasan <arif.cvc13@gmail.com>
2019-01-28 11:26:03 -0600

    remove gitignore folder

---
+++ build/precache-manifest.1f728c305885e17c31c32111e692f7a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99a12efe6e2dec360fdd",
+    "url": "/static/js/main.99a12efe.chunk.js"
+  },
+  {
+    "revision": "44b07b43d8178dcc19b0",
+    "url": "/static/js/1.44b07b43.chunk.js"
+  },
+  {
+    "revision": "99a12efe6e2dec360fdd",
+    "url": "/static/css/main.ce2fda80.chunk.css"
+  },
+  {
+    "revision": "3c6e8f55cf34eed829912d18cb02acd5",
+    "url": "/index.html"
+  }
+];
